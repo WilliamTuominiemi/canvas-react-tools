a0fc2772a8adb1bff17aa961e4f12541d1f43b9c
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2024-02-05 20:44:11 +0200

    set canvas size

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 ### Functions
 
+#### setSize
+
+`Canvas.setBackground(canvasRef.current, 0.8, 0.5)` where the first parameter after the canvas reference is the width and the second is height. The parameters are given as a decimal of the whole screen width.
+
 #### setBackground
 
 `Canvas.setBackground(canvasRef.current, 'red')` where "red" can be altered to choose a color
--- index.js
+++ index.js
@@ -3,6 +3,27 @@
 // import { useEffect, useState, useRef } from 'react'
 
 const Canvas = {
+    setSize: (canvas, newWidth, newHeight) => {
+        try {
+            if (!canvas) {
+                throw new Error('Invalid canvas reference')
+            }
+
+            const screenWidth = window.innerWidth * newWidth
+            const screenHeight = window.innerHeight * newHeight
+
+            const pixelRatio = window.devicePixelRatio || 1
+
+            canvas.width = screenWidth * pixelRatio
+            canvas.height = screenHeight * pixelRatio
+
+            canvas.style.width = `${screenWidth}px`
+            canvas.style.height = `${screenHeight}px`
+        } catch (error) {
+            console.error(error.message)
+        }
+    },
+
     setBackground: (canvas, color) => {
         try {
             if (!canvas) {
